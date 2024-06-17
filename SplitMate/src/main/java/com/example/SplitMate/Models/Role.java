259fc2d87b44b564a6544d19e89d43af62c45ae8
ragz224 <vengalaraghu01@gmail.com>
2024-06-17 21:43:14 +0530

    updated

--- SplitMate/src/main/java/com/example/SplitMate/Models/Role.java
+++ SplitMate/src/main/java/com/example/SplitMate/Models/Role.java
@@ -1,11 +1,15 @@
 package com.example.SplitMate.Models;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Role {
+@Entity
+@Table(name = "roles")
+public class Role extends BaseModel {
     private  String role;
 }
